23da91b4e03d1e04387b0f046aef0df914d5fe41
YangJinSoo94 <jschrome782@gmail.com>
2025-03-05 10:57:13 +0900

    select form 개발중중

--- apps/fo/app/page.tsx
+++ apps/fo/app/page.tsx
@@ -1,5 +1,5 @@
 import type { ChartConfig } from "@common/business_components";
-import { CHART_FORM, MainChart, SearchDiv, Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@common/business_components";
+import { CHART_FORM, MainChart, SearchDiv, SelectBox } from "@common/business_components";
 import { Label } from "@common/business_components/ui/form/label";
 import { Button } from "@common/components/ui";
 
@@ -32,12 +32,19 @@ const chartConfig = chartData.reduce((config,element) => {
 
   }, {} as Record<string, { label: string; color: string; }>) satisfies ChartConfig;
 
-interface SelectBox1 {
-  text:string,
-  val:string
-}
+/**
+ * selectBox data
+ */
+const selectData1 = [
+  { text: "January", val: '00'},
+  { text: "February", val: '01'},
+  { text: "March", val: '03'},
+  { text: "April", val: '04'},
+  { text: "May", val: '05'},
+  { text: "June", val: '06'},
+]
 
-const selectData1 : SelectBox1[] = [
+const selectData2 = [
   { text: "January", val: '00'},
   { text: "February", val: '01'},
   { text: "March", val: '03'},
@@ -52,18 +59,12 @@ export default function Home() {
       <MainChart chartConfig={chartConfig} chartForm={chartForm} chartData={chartData}></MainChart>
       <SearchDiv>
           <Label>ddd</Label>
-          <Select>
-            <SelectTrigger className="w-[100px]">
-              <SelectValue placeholder="Theme" />
-            </SelectTrigger>
-            <SelectContent>
-              {selectData1.map((data) => (
-                <SelectItem key={data.val} value={data.val}>{data.text}</SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-          <Button>
-            search
+          <SelectBox selectArray={selectData1}>
+          </SelectBox>
+          <SelectBox selectArray={selectData2}>
+          </SelectBox>
+          <Button className="h-[2.5rem] w-[1.5rem]">
+            조회
           </Button>
       </SearchDiv>
     </div>
