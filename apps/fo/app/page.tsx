a9ed01ed1ff6d25d16afa7873aba1c2f34a1a595
truthwing <truthplumage@gmail.com>
2025-03-06 13:36:34 +0900

    레이아웃에 추가해서 동작되도록 store context 위치 변경.

--- apps/fo/app/page.tsx
+++ apps/fo/app/page.tsx
@@ -1,10 +1,7 @@
 import DashBoard from "./pages/dashboard/page";
-import { StoreProvider } from "./store";
 
 export default function Home() {
   return (
-    <StoreProvider>
       <DashBoard/>
-    </StoreProvider>
   );
 }
