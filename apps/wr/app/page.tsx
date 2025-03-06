90c36533f400828e254fc165b51f75d1892f36cf
truthwing <truthplumage@gmail.com>
2025-03-06 11:35:36 +0900

    레이아웃이 전체적으로 추가 되도록 수정.

--- apps/wr/app/page.tsx
+++ apps/wr/app/page.tsx
@@ -1,12 +1,9 @@
 import PublicReuseFaciltyInsert from './publicReuseFaciltyInsert/page';
-import { StoreProvider } from './store';
 
 
 
 export default function Home() {
   return (
-    <StoreProvider>
       <PublicReuseFaciltyInsert   />
-    </StoreProvider>
   );
 }
