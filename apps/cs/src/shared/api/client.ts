abc556889376cee055f4688b0f6d82d33677639b
bicochan <chanii8@naver.com>
2025-03-06 19:34:55 +0900

    feat: network 패키지 적용

--- apps/cs/src/shared/api/client.ts
+++ apps/cs/src/shared/api/client.ts
@@ -1,62 +1,7 @@
-import axios from "axios";
-import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import { Client } from "@common/network";
 import { SERVER_API_URL, SITE_ID } from "../config";
 
-class Client {
-  private static instance: Client;
-  private axiosInstance: AxiosInstance;
-
-  private constructor() {
-    this.axiosInstance = axios.create({
-      baseURL: SERVER_API_URL,
-      headers: {
-        "Content-Type": "application/json",
-        "X-Site-Id": SITE_ID,
-      },
-      withCredentials: true,
-    });
-  }
-
-  public static getInstance(): Client {
-    if (!Client.instance) {
-      Client.instance = new Client();
-    }
-    return Client.instance;
-  }
-
-  public async get<T = unknown, R = AxiosResponse<T>>(
-    url: string,
-    config?: AxiosRequestConfig,
-  ): Promise<R> {
-    const response = await this.axiosInstance.get<T, R>(url, config);
-    return response;
-  }
-
-  public async post<T = unknown, R = AxiosResponse<T>, D = unknown>(
-    url: string,
-    data?: D,
-    config?: AxiosRequestConfig<D>,
-  ): Promise<R> {
-    const response = await this.axiosInstance.post<T, R>(url, data, config);
-    return response;
-  }
-
-  public async put<T = unknown, R = AxiosResponse<T>, D = unknown>(
-    url: string,
-    data?: D,
-    config?: AxiosRequestConfig<D>,
-  ): Promise<R> {
-    const response = await this.axiosInstance.put<T, R>(url, data, config);
-    return response;
-  }
-
-  public async delete<T = unknown, R = AxiosResponse<T>>(
-    url: string,
-    config?: AxiosRequestConfig,
-  ): Promise<R> {
-    const response = await this.axiosInstance.delete<T, R>(url, config);
-    return response;
-  }
-}
-
-export const client = Client.getInstance();
+export const client = Client.getInstance({
+  baseURL: SERVER_API_URL as string,
+  siteId: SITE_ID as string,
+});
