811c6e62db81d61007e11d738e7dc5f7cf5417e1
bicochan <chanii8@naver.com>
2025-03-07 18:37:25 +0900

    feat: tree 공통 컴포넌트 생성

--- packages/common/assets/src/images/index.ts
+++ packages/common/assets/src/images/index.ts
@@ -1,6 +1,10 @@
 export const images = {
   logo: require("./logo.png"),
   menuBtn: require("./mob_menuBtn.svg"),
+  menuBtnClose: require("./mob_menuBtn_close.svg"),
+  iconFolderOff: require("./icon_folder_off.png"),
+  iconFolderOn: require("./icon_folder_on.png"),
+  iconFile: require("./icon_file.png"),
 } as const;
 
 export type ImageType = keyof typeof images;
