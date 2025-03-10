bf3e11b241cb53e1ae9a5cdc2c2f19ad65061920
YangJinSoo94 <jschrome782@gmail.com>
2025-03-10 13:34:35 +0900

    대시보드 상단영역까지 commit

---
+++ apps/fo/app/pages/dashboard/facilityInfo/page.tsx
@@ -0,0 +1,121 @@
+﻿"use client"
+import { useFoStore } from "@/app/store";
+import type { TableUpperProps } from "@/app/store/processFacility";
+import { SearchDiv, SelectBox, UiTable } from "@common/business_components";
+import { Button, Input, Label, TableHead, TableRow } from "@common/components/ui";
+
+/** 기본 스타일 */
+const styles = {
+    leftDiv : {
+      width : '90%',
+      margin : '1rem 0rem 1rem 0rem',
+    } as React.CSSProperties,
+    rightDiv : {
+      width : '10%',
+      margin : '1rem 2rem 1rem 0rem',
+    } as React.CSSProperties,
+    searchBtn : {
+      float : 'right',
+      fontSize : '14px',
+      marginRight : '0.8rem'
+    } as React.CSSProperties,
+
+    /** 패키지화 필요 */
+    inputStyleShort : {
+      width:'4rem',
+      height:'2.5rem',
+    } as React.CSSProperties,
+    inputStyleLong : {
+      width:'10rem',
+      height:'2.5rem',
+    } as React.CSSProperties,
+    selectStyle : {
+      marginLeft: '0.5rem',
+      width:'5rem'
+    } as React.CSSProperties,
+  }
+
+const headMakeColSpan = (upHeadList: TableUpperProps[]) => {
+    return  ( <>
+    <TableRow>{
+    upHeadList.map((head: TableUpperProps) => 
+      ( head.id==='publicMethod'?<TableHead rowSpan={2} className="w-1/16" key={head.id}>{head.title}</TableHead>:
+        !head.upSequnce&&head.id!='facilityCapacity'&&head.id!='planInputWaterQlty'&&head.id!='designInputWaterQlty'?<TableHead rowSpan={2}key={head.id}>{head.title}</TableHead>:
+        head.id=='facilityCapacity'||head.id=='planInputWaterQlty'||head.id=='designInputWaterQlty'?<TableHead rowSpan={2} className="w-13" key={head.id}>{head.title}</TableHead>:
+        head.upName === 'upChangeRe'&& head.upSequnce=== 1?<TableHead className="w-75" key={head.id} rowSpan={1} colSpan={2}>{'행정구역'}</TableHead>:null))}
+    </TableRow>
+    <TableRow>
+    {upHeadList.map((head: TableUpperProps, idx) =>{
+      if(head.upName === 'upChangeRe'){
+        if(idx==1){
+          return <TableHead key={head.id} className="w-36">{head.title}</TableHead>;
+        }
+        else{
+          return <TableHead key={head.id} className="w-40">{head.title}</TableHead>;
+        }
+      }
+    })}
+    </TableRow>
+    </> )
+  }
+
+export default function ProcessFacilitySearch() {
+  const {DashboardMain, FcltyMain, FlowRateList} = useFoStore((state) => state);
+
+  // 상단바 관련
+  const {
+    topLabelArray,
+    selectPartData,
+    selectSearchYear,
+    selectSidoData,
+    selectSigunData,
+  } = DashboardMain;
+
+  // fclty 관련
+  const {
+    upHeadListFclty,
+    fcltyList
+  } = FcltyMain;
+
+  // 그리드 관련
+  const {
+    upHeadList, 
+    flowRateList
+  } = FlowRateList;
+
+  const upChangeRe= headMakeColSpan(upHeadList);
+
+  return (
+      <div className="m-8">
+        <SearchDiv>
+            <div style={styles.leftDiv} className="grid grid-cols-12 gap-1">
+              <SelectBox className="col-span-2" label={topLabelArray[0] as string} selectArray={selectPartData}>
+              </SelectBox>
+              <SelectBox className="col-span-2" label={topLabelArray[1] as string} selectArray={selectSidoData}>
+              </SelectBox>
+              <SelectBox className="col-span-2" label={topLabelArray[2] as string} selectArray={selectSigunData}>
+              </SelectBox>
+              <SelectBox className="col-span-2" label={topLabelArray[3] as string} selectArray={selectSearchYear}>
+              </SelectBox>
+              <div className="col-span-2 flex" >
+                {topLabelArray[4] && <Label className="m-4 text-xs w-1/4">{topLabelArray[4]}</Label>}
+                <Input style={styles.inputStyleLong}></Input>
+              </div>
+            </div>
+            <div style={styles.rightDiv}>
+              <Button style={styles.searchBtn} size="sm">
+                초기화
+              </Button>
+              <Button style={styles.searchBtn} size="sm">
+                조회
+              </Button>
+            </div>
+        </SearchDiv>
+        <UiTable headName={""} publicReuseFacility={fcltyList} headlist={upHeadListFclty} pageSize={0} total={0} children={undefined}>
+        </UiTable>
+        <UiTable headName={""} publicReuseFacility={flowRateList} headlist={upHeadList} pageSize={8} total={16}>
+          {upChangeRe}
+        </UiTable>
+      </div>
+  );
+}
--- apps/fo/app/store/dashboard/index.tsx
+++ apps/fo/app/store/dashboard/index.tsx
@@ -1,63 +1,242 @@
+/**
+ * Store - 타입
+ */
 
+// selectBox
 export type SelectDataType = {
     text: string;
     val: string;
 }
-const selectData1:SelectDataType[] = [
+
+// grid header
+export type TableUpperProps = {
+    id: string;
+    title: string;
+    upName?: string;
+    upSequnce?: number;
+}
+
+// main fclty
+export type mainFcltyProps = {
+    fcltyNm : string,
+    facilityCapacity : string,
+    publicMethod : string,
+    location : string
+}
+
+// list1
+export type flowRateListProps = {
+    facilityCd : string,
+    sido : string,
+    sigungo : string,
+    facilityName : string,
+    location : string,
+    facilityCapacity : string,
+    totInflow : string,
+    sewageInflow : string,
+    linkedTreatedWater : string,
+    totReturnWater : string,
+    discharge : string,
+}
+
+/**
+ * Store - data
+ */
+
+// Main
+const DashBoardTopLabelArray:string[] = ['구분', '시도', '시군구', '년도', '시설명'];
+const selectPartData:SelectDataType[] = [
     { text: "행정별", val: '00'},
-    { text: "구역별", val: '01'},
+    { text: "유역별", val: '01'},
+    { text: "환경청별", val: '02'},
+];
+const selectSearchYear:SelectDataType[] = [
+    { text: "2025년", val: '2025'},
+    { text: "2024년", val: '2024'},
+    { text: "2023년", val: '2023'},
+];
+const selectSidoData:SelectDataType[] = [
+    { text: "전체", val: '00'},
+    { text: "서울특별시", val: '01'},
+    { text: "인천광역시", val: '02'},
+    { text: "경기도", val: '03'},
+    { text: "강원도", val: '04'},
+    { text: "충청남도", val: '05'},
+    { text: "충청북도", val: '06'},
+    { text: "경상남도", val: '07'},
+    { text: "경상북도", val: '08'},
+    { text: "전라남도", val: '09'},
+    { text: "전라북도", val: '10'},
+    { text: "제주도", val: '11'},
+];
+const selectSigunData:SelectDataType[] = [
+    { text: "전체", val: '00'},
+    { text: "동대문구", val: '01'},
+    { text: "여의도", val: '02'},
+    { text: "강남구", val: '03'},
+    { text: "노원구", val: '04'},
+    { text: "영등포구", val: '05'},
+    { text: "관악구", val: '06'},
+    { text: "광진구", val: '07'},
+    { text: "망원동", val: '08'},
+];
+
+// grid header
+const upHeadListFclty:TableUpperProps[] = [
+    { id : "fcltyNm", title : "시설명"},
+    { id : "facilityCapacity", title : "시설용량(m³/L)"},
+    { id : "publicMethod", title : "공법"},
+    { id : "location", title : "지역구"},
 ]
 
-const selectData2:SelectDataType[] = [
-    { text: "January", val: '00'},
-    { text: "February", val: '01'},
-    { text: "March", val: '03'},
-    { text: "April", val: '04'},
-    { text: "May", val: '05'},
-    { text: "June", val: '06'},
+const upHeadList:TableUpperProps[] = [
+    { id: "facilityCd", title: "시설코드"},
+    { id: "sido", title: "시도", upName:"upChangeRe", upSequnce:1 },
+    { id: "sigungo", title: "시군구", upName:"upChangeRe",upSequnce:2 },
+    { id: "facilityName", title: "시설명"},
+    { id: "location", title: "주소"},
+    { id: "facilityCapacity", title: "시설용량(m³/L)"},
+    { id: "totInflow", title: "총 유입입량(m³/일)"},
+    { id: "sewageInflow", title: "하수유입량(m³/일)"},
+    { id: "linkedTreatedWater", title: '연계처리량(m³/일)'},
+    { id: "totReturnWater", title: '총인 반류수량(m³/일)'},
+    { id: "discharge", title: '방류량(m³/일)'}
+];
+
+// main fclty
+const fcltyList : mainFcltyProps[] = [
+    {
+        fcltyNm : "난지",
+        facilityCapacity : "860,000",
+        publicMethod : "MLE, A2O",
+        location : "500톤 이상(Ⅲ지역)"
+    }
 ]
 
-const labelArray:string[] = ['구분', '시도', '시군구'];
+// list1
+const flowRateList:flowRateListProps[] = [
+    {
+        facilityCd : "11000SW001R",
+        sido: "서울특별시",
+        sigungo: "강남구",
+        facilityName: "난지",
+        location: "경기도 고양시 덕양구 대차로 4가",
+        facilityCapacity: "860,000",
+        totInflow : "547,637",
+        sewageInflow : "539,519.4",
+        linkedTreatedWater : "699.0",
+        totReturnWater: "7,418.6",
+        discharge: "547,637.0"
+    },
+    {
+        facilityCd : "11200PB001R",
+        sido: "서울특별시",
+        sigungo: "성동구",
+        facilityName: "중량물재생센터",
+        location: "서울특별시 성동구 자동차시장3길 64 (용답동, 중랑물재생센터)",
+        facilityCapacity: "1,590,000",
+        totInflow : "1,003,563",
+        sewageInflow : "883,231.8",
+        linkedTreatedWater : "634.9",
+        totReturnWater: "8631.0",
+        discharge: "545,234.8"
+    },
+    {
+        facilityCd : "11500PB001R",
+        sido: "서울특별시시",
+        sigungo: "강서구",
+        facilityName: "서남",
+        location: "서울특별시 강서구 마곡동 74",
+        facilityCapacity: "1,630,000",
+        totInflow : "1,445,233",
+        sewageInflow : "1,113,443.2",
+        linkedTreatedWater : "881.8",
+        totReturnWater: "9882.0",
+        discharge: "632,194"
+    },
+    {
+        facilityCd : "11680PB001R",
+        sido: "서울특별시",
+        sigungo: "강남구",
+        facilityName: "탄천",
+        location: "서울특별시 강남구 일원동 580",
+        facilityCapacity: "900,000",
+        totInflow : "467,343",
+        sewageInflow : "443,321",
+        linkedTreatedWater : "322",
+        totReturnWater: "3,123",
+        discharge: "323,123"
+    },
+]
+
+export type DashBoardActions = {
+    MainAction: {
+        labelChange:()=>void;
+    },
+}
 
 export type DashBoardType = {
-    DashBoard: {
+    DashboardMain: {
         isInit: boolean;
-        selectData1: SelectDataType[];
-        selectData2: SelectDataType[];
-        labelArray:string[];
+        topLabelArray:string[];
+        selectPartData:SelectDataType[];
+        selectSearchYear:SelectDataType[];
+        selectSidoData:SelectDataType[];
+        selectSigunData:SelectDataType[];
+    },
+
+    FcltyMain : {
+        isInit : boolean;
+        upHeadListFclty : TableUpperProps[];
+        fcltyList : mainFcltyProps[];
+    },
+
+    FlowRateList : {
+        isInit : boolean;
+        upHeadList: TableUpperProps[];
+        flowRateList:flowRateListProps[];
     }
 }
-export type DashBoardActions = {
-    DashBoardActions: {
-        labelChange:()=>void;
-    }
-}
+
 export type DashBoardStore = DashBoardType & DashBoardActions;
 
 export const dashInitState:DashBoardType = {
-    DashBoard: {
+    DashboardMain: {
         isInit: false,
-        selectData1: selectData1,
-        selectData2: selectData2,
-        labelArray: labelArray
+        topLabelArray: DashBoardTopLabelArray,
+        selectPartData : selectPartData,
+        selectSearchYear : selectSearchYear,
+        selectSidoData : selectSidoData,
+        selectSigunData: selectSigunData,
+    },
+    
+    FcltyMain : {
+        isInit : false,
+        upHeadListFclty : upHeadListFclty,
+        fcltyList : fcltyList,
+    },
+
+    FlowRateList : {
+        isInit : false,
+        upHeadList : upHeadList,
+        flowRateList : flowRateList
     }
 }
 
 export const dashBoardReducer:(set:any)=>DashBoardActions=(set: any) => {
-
     console.log("tableActionsExport");
     return {
-        DashBoardActions: {
+        MainAction: {
             labelChange:()=>set(
                 (state:DashBoardType) => {
-                    state.DashBoard.labelArray[0] = "구분 변경";
+                    state.DashboardMain.topLabelArray[0] = "구분 변경";
                 return (
                 { 
-                    DashBoard: {
-                        ...state.DashBoard,
-                        labelArray: state.DashBoard.labelArray
+                    FlowRateSearch: {
+                        ...state.DashboardMain,
+                        topLabelArray: state.DashboardMain.topLabelArray
                 }})}
             )
-        }
+        },
     }
 }
