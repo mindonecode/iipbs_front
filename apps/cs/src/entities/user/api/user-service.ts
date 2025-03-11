2f8f1b5ddccf4f033aa1d9124663839d06d5a54e
bicochan <chanii8@naver.com>
2025-03-11 16:20:45 +0900

    feat: client request 인증 로직 추가

--- apps/cs/src/entities/user/api/user-service.ts
+++ apps/cs/src/entities/user/api/user-service.ts
@@ -1,18 +1,11 @@
 import { client } from "@/shared/api";
-import { getToken } from "@/shared/lib/cookie";
-import { AUTH_USER_ID, CLAIM_NAME, ENDPOINT } from "@/shared/config";
-import type { IUser, IUserInfo, UserService } from "../model/user";
+import { ENDPOINT } from "@/shared/config";
+import type { IUser, IUserInfo, UserService } from "../model/user-interface";
 
 export class UsersApiService implements UserService {
   public async userInfo(payload: IUserInfo): Promise<IUser> {
     const response = await client.get<IUser>(
       `${ENDPOINT.USER_SERVICE.USERS}/${payload.userUniqId}`,
-      {
-        headers: {
-          [CLAIM_NAME]: `${getToken(CLAIM_NAME)}`,
-          [AUTH_USER_ID]: `${getToken(AUTH_USER_ID)}`,
-        },
-      },
     );
 
     return response.data;
