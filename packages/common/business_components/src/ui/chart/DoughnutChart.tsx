396992fe1bab12ecc8461ae3150e99c84cd88317
YangJinSoo94 <jschrome782@gmail.com>
2025-03-12 16:36:25 +0900

    도넛차트 레이아웃 수정정

--- packages/common/business_components/src/ui/chart/DoughnutChart.tsx
+++ packages/common/business_components/src/ui/chart/DoughnutChart.tsx
@@ -17,28 +17,30 @@ import {
 export default (props: { chartConfig: ChartConfig; chartData: never[] | undefined; chartTitle:string}) =>{
   return (
     <Card className="flex flex-col">
+      <CardContent className="flex-1 pb-0">
       {props.chartTitle && props.chartTitle !== "" && props.chartTitle !== "undefined" ? (
         <div style={{ textAlign: "left", fontSize: "13px", fontWeight: "bold", marginTop:"13px", marginBottom: "10px" }}>
           {props.chartTitle}
         </div>): null}
-      <CardContent className="flex-1 pb-0">
-        <ChartContainer
-          config={props.chartConfig}
-          className="mx-auto aspect-square max-h-[300px]"
-        >
-          <PieChart>
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent hideLabel />}
-            />
-            <Pie
-              data={props.chartData}
-              dataKey="val"
-              nameKey="label"
-              innerRadius={60}
-            />
-          </PieChart>
-        </ChartContainer>
+        <div className="h-[calc(100%-42px)] justify-center">
+          <ChartContainer
+            config={props.chartConfig}
+            className="mx-auto aspect-square max-h-[300px]"
+          >
+            <PieChart>
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Pie
+                data={props.chartData}
+                dataKey="val"
+                nameKey="label"
+                innerRadius={60}
+              />
+            </PieChart>
+          </ChartContainer>
+        </div>
       </CardContent>
     </Card>
   )
