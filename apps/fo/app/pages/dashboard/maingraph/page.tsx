ef58444d718079b784a2a0bd982124539ba20e6b
YangJinSoo94 <jschrome782@gmail.com>
2025-03-12 11:00:27 +0900

    대시보드 메인그래프 화면개발 진행중중

---
+++ apps/fo/app/pages/dashboard/maingraph/page.tsx
@@ -0,0 +1,44 @@
+﻿"use client"
+import { useFoStore } from "@/app/store";
+import { CHART_FORM, type ChartConfig, MainChart } from "@common/business_components";
+
+  const chartData = [
+    {label : 'a', val : '123'},
+    {label : 'b', val : '143'},
+    {label : 'c', val : '163'}
+  ];
+
+/**
+ * 차트 Config
+ */
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig;
+
+export default function ProcessFacilitySearch() {
+  const {MainGraph} = useFoStore((state) => state);
+
+  const {
+    chart_inflowRain
+  } = MainGraph;
+
+  return (
+      <div className="grid grid-cols-12 gap-1">
+        <div className="col-span-6">
+          <MainChart chartForm={CHART_FORM.LINEBAR} chartConfig={chartConfig} chartData={chart_inflowRain}></MainChart>
+        </div>
+        <div className="col-span-6">
+          <MainChart chartForm={CHART_FORM.LINE} chartConfig={chartConfig} chartData={chartData}></MainChart>
+        </div>
+        <div className="col-span-6">
+          <MainChart chartForm={CHART_FORM.LINE} chartConfig={chartConfig} chartData={chartData}></MainChart>
+        </div>
+        <div className="col-span-6">
+          <MainChart chartForm={CHART_FORM.LINE} chartConfig={chartConfig} chartData={chartData}></MainChart>
+        </div>
+      </div>
+  );
+}
