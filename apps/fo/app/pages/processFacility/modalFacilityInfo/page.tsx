b841b57b9edeb801ec6e2465b6e94a4f07d724c9
YangJinSoo94 <jschrome782@gmail.com>
2025-03-18 10:55:09 +0900

    처리시설 정보 모달 작업 진행중

--- apps/fo/app/pages/processFacility/modalFacilityInfo/page.tsx
+++ apps/fo/app/pages/processFacility/modalFacilityInfo/page.tsx
@@ -4,6 +4,12 @@ import type { TableUpperProps } from "@/app/store/dashboard";
 import { UiTable } from "@common/business_components";
 import { Table, TableCell, TableHead, TableRow } from "@common/components/ui";
 import { useRef } from "react";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@common/components";
 
 // 시설계획 table header 변경
 const headMakeList = (upHeadList: TableUpperProps[], gridNum:number) => {
@@ -62,15 +68,31 @@ export default function ProcessFacilityInfoModal(props: { fcltyName: string; fac
           <TableCell className="w-1/3">{props.facilityCd}</TableCell>
         </TableRow>
       </Table>
-      <div style={{ textAlign: "left", fontSize: "13px", fontWeight: "bold", marginTop:"13px", marginBottom: "10px" }}>
-        {'시설증설 계획'}
-      </div>
-      <UiTable headName={undefined} tableData={gridListExplainPlan} headlist={upHeadListExplainPlan} pageSize={0} total={0}>
-        {upHeadListExplainPlanChange}
-      </UiTable>
-      <UiTable headName={undefined} tableData={gridListExplainPlan} headlist={upHeadListExplainPlan} pageSize={0} total={0}>
-        {upHeadListExplainPlanChange}
-      </UiTable>
+      <Tabs defaultValue="common_info" className="w-full">
+        <TabsList className="grid grid-cols-2">
+          <TabsTrigger value="common_info">기본정보</TabsTrigger>
+          <TabsTrigger value="history_info">이력정보</TabsTrigger>
+        </TabsList>
+        <TabsContent value="common_info">
+          <Table>
+            <TableRow className="">
+              <TableHead className="w-1/6">{'시설명'}</TableHead>
+              <TableCell className="w-1/3">{gridListExplainPlan[0]?.facilityName}</TableCell>
+              <TableHead className="w-1/6">{'시설용량'}</TableHead>
+              <TableCell className="w-1/3">{gridListExplainPlan[0]?.facilityCapacity}</TableCell>
+            </TableRow>
+            <TableRow className="">
+              <TableHead className="w-1/6">{'시도'}</TableHead>
+              <TableCell className="w-1/3">{gridListExplainPlan[0]?.sido}</TableCell>
+              <TableHead className="w-1/6">{'시설용량'}</TableHead>
+              <TableCell className="w-1/3">{gridListExplainPlan[0]?.facilityCapacity}</TableCell>
+            </TableRow>
+          </Table>
+        </TabsContent>
+        <TabsContent value="history_info">
+          
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
