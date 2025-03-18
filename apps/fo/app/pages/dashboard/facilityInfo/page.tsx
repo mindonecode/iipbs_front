46f85f4be3bbe5831e49d10aec5c1b53e301e71b
YangJinSoo94 <jschrome782@gmail.com>
2025-03-18 15:47:59 +0900

    UI테이블 호출 파라미터 수정정
    Former-commit-id: fcba19dfd47462f3e799c67731965e366800ee5d

--- apps/fo/app/pages/dashboard/facilityInfo/page.tsx
+++ apps/fo/app/pages/dashboard/facilityInfo/page.tsx
@@ -103,7 +103,7 @@ export default function ProcessFacilitySearch() {
             </Button>
           </div>
         </SearchDiv>
-        <UiTable headName={""} tableData={fcltyList} headlist={upHeadListFclty} pageSize={0} total={0} cellClick={function (index: number): void | undefined{} } children={undefined}/>
+        <UiTable headName={""} tableData={fcltyList} headlist={upHeadListFclty} pageSize={0} total={0} cellClick={()=>{} } children={()}/>
         <div className="grid grid-cols-12 gap-1">
           <div className="col-span-5">
              <Table>
