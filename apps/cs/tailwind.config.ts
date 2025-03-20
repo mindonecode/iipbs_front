9e1ae9a708f19e6ec275513c5c3440961139c058
bicochan <chanii8@naver.com>
2025-03-20 18:50:31 +0900

    feat: 다크 모드 적용
    Former-commit-id: b8e0a8a878a04508eda282eb05c690522b2e31ab

--- apps/cs/tailwind.config.ts
+++ apps/cs/tailwind.config.ts
@@ -4,7 +4,6 @@ import presetConfig from "../../tailwind.config.preset";
 
 export default {
   presets: [presetConfig],
-  darkMode: ["class", "[data-mode='dark']"],
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "../../packages/common/components/src/ui/**/*.{js,ts,jsx,tsx,mdx}",
