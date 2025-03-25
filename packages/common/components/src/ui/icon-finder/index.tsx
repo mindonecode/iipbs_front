9039b50b74057cd2b1175491d18475fee69033a4
bicochan <chanii8@naver.com>
2025-03-25 09:48:14 +0900

    feat: icon finder 검색 기능 추가
    Former-commit-id: 8f4dc1bd34732d4d77fc9ac5cd5f947e46e6da6b

--- packages/common/components/src/ui/icon-finder/index.tsx
+++ packages/common/components/src/ui/icon-finder/index.tsx
@@ -1,19 +1,34 @@
 import { useState } from "react";
-import { Button } from "../button";
-import { Dialog, DialogContent, DialogTrigger } from "../dialog";
 import { cn } from "../../lib";
+import { Button } from "../button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../dialog";
+import { Input } from "../input";
 import iconNames from "./data";
 
 type IconFinderProps = {
-  handleClick: (icon: string) => void;
+  handleClick?: (icon: string) => void;
 };
 
 const IconFinder = ({ handleClick }: IconFinderProps) => {
   const [selectedIcon, setSelectedIcon] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [filteredIconNames, setFilteredIconNames] = useState(iconNames);
 
   const handleIconClick = (icon: string) => {
     setSelectedIcon(icon);
-    handleClick(icon);
+    handleClick?.(icon);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInputValue(value);
+    setFilteredIconNames(iconNames.filter((icon) => icon.includes(value)));
   };
 
   return (
@@ -24,10 +39,19 @@ const IconFinder = ({ handleClick }: IconFinderProps) => {
           아이콘 찾기
         </Button>
       </DialogTrigger>
-      <DialogContent className="max-h-[65rem] max-w-[65rem] overflow-y-auto p-12">
-        <div className="py-4">
+      <DialogContent className="flex h-[65rem] max-w-[65rem] flex-col items-start overflow-y-auto">
+        <DialogHeader className="w-full">
+          <DialogTitle>아이콘 찾기</DialogTitle>
+        </DialogHeader>
+        <div className="flex w-full flex-col gap-4">
+          <Input
+            value={inputValue}
+            onChange={handleInputChange}
+            className="!text-[1.3rem] text-foreground"
+            placeholder="아이콘 이름 검색"
+          />
           <div className="grid grid-cols-5 gap-6 text-[2.4rem]">
-            {iconNames.map((icon) => (
+            {filteredIconNames.map((icon) => (
               <IconItem
                 key={icon}
                 icon={icon}
@@ -65,8 +89,8 @@ const IconItem = ({
           selectedIcon !== icon && "hidden",
         )}
       />
-      <i className={`diveicon di-${icon}`} />
-      <span className="text-center text-[1.2rem]">{icon}</span>
+      <i className={`diveicon di-${icon} text-foreground`} />
+      <span className="text-center text-[1.2rem] text-foreground">{icon}</span>
     </div>
   );
 };
