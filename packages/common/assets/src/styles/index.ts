9ae67f44710cd9bc4f65ed2bbc641e6f86f19b53
최지철 <jcchoi@onasys.co.kr>
2025-03-26 14:05:59 +0900

    chore : 공통 components.css 추가가
    Former-commit-id: 59c57d45d52cfd07e2d2e4b8b2df45333b990a51

--- packages/common/assets/src/styles/index.ts
+++ packages/common/assets/src/styles/index.ts
@@ -2,6 +2,7 @@ export const styles = {
   base: require("./base.css"),
   diveicon: require("./diveicon.css"),
   grid: require("./grid.css"),
+  components: require("./components.css"),
 } as const;
 
 export type StyleType = keyof typeof styles;
